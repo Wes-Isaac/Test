3cd451a250ee1ad2e04832c5dfda4608ad9a03c6
unknown <yisehakmitiku@gmail.com>
2021-10-04 23:25:18 +0800

    edit mobile

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const hamburger = document.querySelector('.fa-bars');
 const menu = document.querySelector('#menu');
 const menuLis = menu.querySelectorAll('li');
+const main = document.querySelector('main');
 
 hamburger.addEventListener('click', () => {
   menu.classList.replace('invisible', 'menu');
+  main.style.backgroundColor = '#d0d9d4';
 });
 for (let i = 0; i < menuLis.length; i = i + 1) {
   menuLis[i].addEventListener('click', () => {
     menu.classList.replace('menu', 'invisible');
+    main.style.backgroundColor = '#fff';
+
   });
 }
